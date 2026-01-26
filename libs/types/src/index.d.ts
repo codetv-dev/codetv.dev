8c6f563fa2ae50b8c6827d8745eb7405069ebf34
Jason Lengstorf <jason@codetv.dev>
2026-01-26 12:09:45 -0800

    feat: support extras + compact layout
    yes this should have been multiple PRs. no I'm not going to fix it

--- libs/types/src/index.d.ts
+++ libs/types/src/index.d.ts
@@ -1,5 +1,7 @@
 export * from './sanity.ts';
 
+export type UnwrapArray<A> = A extends unknown[] ? UnwrapArray<A[number]> : A;
+
 export type SubscriptionLevel =
 	| 'Free Tier Supporter'
 	| 'Silver Tier Supporter'
